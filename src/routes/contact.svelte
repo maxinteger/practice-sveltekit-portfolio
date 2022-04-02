ada522faeb3918e896b2b8c0c93fb4a0a21803a1
Vadasz Laszlo <maxinteger@gmail.com>
2022-04-02 16:17:12 +0100

    Initial commit

---
+++ src/routes/contact.svelte
@@ -0,0 +1,16 @@
+<!-- src/routes/contact.svelte -->
+<svelte:head>
+    <title>Contact</title>
+</svelte:head>
+
+<div class="container">
+    <p>Try experimenting Sveltekit by developing this page with your knowledge</p>
+</div>
+
+
+<style>
+    .container {
+        max-width: 800px;
+        margin: 50px auto;
+    }
+</style>
